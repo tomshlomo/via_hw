bdf9997d54e9f989ccec38147c0b5643d1a08bc7
Tom Shlomo <tomshlomo@gmail.com>
2020-12-30 18:13:44 +0200

    wrapped load data in a function

--- data.py
+++ data.py
@@ -2,15 +2,12 @@ import pandas as pd
 import numpy as np
 
 
-req = pd.read_csv('requests.csv')
-dur = pd.read_csv('durations.csv')
-# dur_mat = np.loadtxt('durations.csv', delimiter=',', skiprows=1)
-dur_mat = np.genfromtxt('durations.csv', delimiter=',')
-dur_mat = dur_mat[1:, 1:]
+def load(requests_file='requests.csv', durations_file='durations.csv'):
+    req = pd.read_csv(requests_file)
+    dur_mat = np.genfromtxt(durations_file, delimiter=',')
+    dur_mat = dur_mat[1:, 1:]
 
-
-req['duration'] = dur_mat[req['pickup'], req['dropoff']]
-req['t_start'] = req['ts'] - req['ts'].min()
-req['t_end'] = req['t_start'] + req['duration']
-
-pass
+    req['duration'] = dur_mat[req['pickup'], req['dropoff']]
+    req['t_start'] = req['ts'] - req['ts'].min()
+    req['t_end'] = req['t_start'] + req['duration']
+    return req, dur_mat
